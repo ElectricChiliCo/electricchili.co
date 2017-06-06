f08e8fab2316e4e7222ca3e0fdb4aa1241d744cb
Andrew Carmer <andrew@gospotcheck.com>
2017-06-05 23:52:34 -0600

    update work

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,50 +1,53 @@
 // Freelancer Theme JavaScript
 
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 100)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('.page-scroll a').bind('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 100)
+      }, 1250, 'easeInOutExpo');
+      event.preventDefault();
+
+      $('#mainNav .logo').animate({
+        scrollRight: ($('.logo').offset().right - 100)
+      }, 1250, 'easeInOutExpo');
+    }
+  );
+
+// Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 100
+  });
+
+// Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
+  });
+
+// Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
+// Floating label headings for the contact form
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
     });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){
-            $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
+  });
 
 
-
-
-  $("div.ecc-tab-menu>div.list-group>a").click(function(e) {
+  $("div.ecc-tab-menu>div.list-group>a").click(function (e) {
     e.preventDefault();
     $(this).siblings('a.active').removeClass("active");
     $(this).addClass("active");
@@ -54,12 +57,12 @@
   });
 
 
-console.log("win")
+  console.log("win")
 
-})(jQuery); // End of use strict
+})
+(jQuery); // End of use strict
 
 
-
-$(document).ready(function() {
+$(document).ready(function () {
 
 });
